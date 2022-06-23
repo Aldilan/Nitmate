86e9ea849e91c02fcc1f0f54fda01026a6fa6e15
Aldilan <aldilan108@gmail.com>
2022-06-23 23:43:06 +0700

    UI & UX

---
+++ assets/js/script.js
@@ -0,0 +1,99 @@
+//AOS
+AOS.init();
+
+//Menu pages
+
+const menuBtn = document.getElementById('menuBtn')
+const menu = document.getElementById('menu')
+menuBtn.addEventListener('mouseover', function (e) {
+    menu.classList.remove('hidden')
+})
+menuBtn.addEventListener('mouseout', function (e) {
+    menu.classList.add('hidden')
+})
+
+menu.addEventListener('mouseover', function (e) {
+    menu.classList.remove('hidden')
+})
+menu.addEventListener('mouseout', function (e) {
+    menu.classList.add('hidden')
+})
+
+
+//Dark mode
+const modeTgl = document.getElementById('modeTgl')
+const modeTglCrl = document.getElementById('modeTglCrl')
+const htmlTag = document.querySelector('html')
+const sunIco = document.getElementById('sun')
+const moonIco = document.getElementById('moon')
+
+modeTgl.addEventListener('click', function (e) {
+    modeTgl.classList.toggle("border-amber-400",)
+    modeTgl.classList.toggle("bg-amber-400")
+    modeTgl.classList.toggle("border-[#3686A0]",)
+    modeTgl.classList.toggle("bg-[#3686A0]")
+    modeTglCrl.classList.toggle('translate-x-0')
+    modeTglCrl.classList.toggle('translate-x-full')
+    htmlTag.classList.toggle('dark')
+    sunIco.classList.toggle('text-amber-400')
+    sunIco.classList.toggle('text-white')
+    moonIco.classList.toggle('text-slate-200')
+    moonIco.classList.toggle('text-[#3686A0]')
+})
+
+//Format Function
+const downFormats = document.querySelectorAll('#downFormat span')
+const downVideo = document.getElementById('downVideo')
+const downAudio = document.getElementById('downAudio')
+const downBefore = document.getElementById('downBefore')
+downFormats.forEach(function name(downFormat) {
+    downFormat.addEventListener('click', function (e) {
+        downFormats.forEach(function (downFormat) {
+            downFormat.classList.remove('hover:text-black')
+            downFormat.classList.add('hover:text-[#B20600]')
+            downFormat.classList.remove('text-white')
+        })
+        downBefore.classList.remove('translate-x-0')
+        downBefore.classList.add('-translate-x-full')
+        downFormat.parentElement.classList.remove('before:-translate-x-full')
+        e.target.classList.remove('hover:text-[#B20600]')
+        e.target.classList.add('hover:text-black')
+        e.target.classList.add('text-white')
+        if (e.target.innerHTML == 'Video') {
+            downFormat.parentElement.classList.add('before:translate-x-0')
+            downFormat.parentElement.classList.remove('before:translate-x-full')
+            downVideo.classList.remove('-translate-x-full')
+            downAudio.classList.add('-translate-x-full')
+        }else{
+            downFormat.parentElement.classList.remove('before:translate-x-0')
+            downFormat.parentElement.classList.add('before:translate-x-full')
+            downAudio.classList.remove('-translate-x-full')
+            downVideo.classList.add('-translate-x-full')
+        }
+    })
+})
+
+//Paste func
+const pasteBtn = document.getElementById('pasteBtn')
+const urlInput = document.getElementById('urlInput')
+let pasteData
+pasteBtn.addEventListener('click', async () => {
+    pasteData = await navigator.clipboard.readText()
+    urlInput.setAttribute('value',pasteData)
+})
+
+//Download Function
+// let downLink, downFormatOption
+// const downOptions = document.querySelectorAll('.downOption')
+// const btnContainer = document.querySelector('#btnContainer')
+// downOptions.forEach(function (downOption){
+//     downOption.addEventListener('click',function () {
+//         downLink = 'https://www.youtube.com/watch?v=gpLJKNPtKB8'
+//         downFormatOption = downOption.value
+//         downFunc(downLink, downFormatOption)
+//     })
+// })
+
+// function downFunc(link, format) {
+//     btnContainer.setAttribute('src','https://loader.to/api/button/?url'+link+'&f='+format)
+// }
---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  content: ["./*.{html,js}"],
+  darkMode : 'class',
+  theme: {
+    extend: {
+      fontFamily: {
+        'kanit': ['Kanit'],
+        'ubuntu': ['Ubuntu'],
+        'montserrat': ['Montserrat'],
+      }
+    },
+  },
+  plugins: [],
+}
+
+
+  
